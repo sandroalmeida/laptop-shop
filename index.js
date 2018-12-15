458c580805863a6675a7199bb817bf5b3c147990
sandroalmeida <sandrodotbuild@gmail.com>
2018-12-15 13:13:02 -0800

    Implemented fs http url modules and route rules from pages

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if(pathName === '/products' || pathName === '/'){
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.end('This is the PRODUCTS page!');
+
+    }else if(pathName === '/laptop' && id < laptopData.length){
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.end(`This is the LAPTOP page ${id}!`);
+
+    }else{
+        res.writeHead(404, {'Content-type':'text/html'});
+        res.end('Page NOT found in the server!');
+
+    }
+
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening requests now.');
+});
